95fafcfebb9a9a2dcbe78c2bb7fe97cc08ef41c8
Jake <jaka.presecnik@gmail.com>
2020-12-19 21:26:42 +0100

    added a function to calculate night hours - needs to be rearanged along with other functions

--- src/app/oseba-vrstica/oseba-vrstica.component.ts
+++ src/app/oseba-vrstica/oseba-vrstica.component.ts
@@ -36,6 +36,43 @@ export class OsebaVrsticaComponent implements OnInit {
       return `${hours}:${minutes}`;
     }
   }
+
+  getNocne(sD: Date, eD: Date) {
+    let ura: number = sD.getHours();
+    let minute: number = sD.getMinutes();
+    let uraDo: number = eD.getHours();
+
+    let dvaDni: boolean = !!(eD.getDate()-sD.getDate());
+    
+    let hNocna: number = 0;
+    let mNocna: number = 0;
+
+    for(; ura < 25; ura++) {
+      if (ura >= uraDo && !dvaDni) {
+        break;
+      }else if(ura < 22 && ura >= 6) {
+        continue;
+      }else{
+        if(minute !== 0 && uraDo > 6 && uraDo < 22) {
+          mNocna = +minute;
+          minute = 0;
+        }else {
+          hNocna++;
+        }
+      }
+
+      if(ura === 24) {
+        ura = 0;
+        dvaDni = !dvaDni;
+      }
+    }
+    return hNocna+ ':' + mNocna;
+  }
+
+  isSunday() {
+      return new Date(this.data.od).getDay() === 0;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/oseba.service.ts
+++ src/app/oseba.service.ts
@@ -14,13 +14,17 @@ export class OsebaService {
       days: days
     }
   }
-  
+
   getMesecOsebaData(y, m, emso) {
     return {
       ime: "Jaka Presečnik",
       data: [
+        {od: "2020-12-01T22:30", do: "2020-12-01T23:30", visinska: 0, dopust: false, bolniska: false },
+        {od: "2020-12-02T22:30", do: "2020-12-03T07:30", visinska: 0, dopust: false, bolniska: false },
+        {od: "2020-12-03T21:30", do: "2020-12-04T05:00", visinska: 0, dopust: false, bolniska: false },
         {od: "2020-12-15T12:30", do: "2020-12-15T14:00", visinska: 0, dopust: false, bolniska: false },
         {od: "2020-12-16T01:30", do: "2020-12-16T03:30", odd: "2020-12-16T07:30", dod: "2020-12-16T18:00", visinska: 0, dopust: false, bolniska: false },
+        {od: "2020-12-20T01:30", do: "2020-12-20T03:30", odd: "2020-12-20T07:30", dod: "2020-12-20T18:00", visinska: 0, dopust: false, bolniska: false },
       ]
     }
   }
